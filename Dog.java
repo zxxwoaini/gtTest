e39a4f3a52dc00ed97caacdb6d85b9ea7e7a5092
woaini995 <vip929588@126.com>
2018-04-07 21:30:28 +0800

    ewfefewrfew

---
+++ Dog.java
@@ -0,0 +1,27 @@
+package com.gugu.pojo;
+
+/**
+ * Created by Administrator on 2018/4/6.
+ */
+public class Dog {
+
+    private  String dogname;
+
+    private String hoster;
+
+    public String getDogName() {
+        return dogname;
+    }
+
+    public void setDogName(String dogName) {
+        this.dogname = dogName;
+    }
+
+    public String getHoster() {
+        return hoster;
+    }
+
+    public void setHoster(String hoster) {
+        this.hoster = hoster;
+    }
+}
